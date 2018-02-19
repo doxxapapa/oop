3dc4e86d7e6d3626881fe30bed2b68d2928caaf3
molnar76 <molnar76@lab104-17.iit.uni-miskolc.hu>
2018-02-19 16:17:32 +0100

    Wallis formula implementalva

---
+++ ElsoGyak/src/Leibniz.java
@@ -0,0 +1,16 @@
+
+public class Leibniz {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		double szamlalo=1;
+		int darab =1000 / 2;
+		for (int i = 1; i < darab; i++) {
+			 
+		}
+
+	}
+
+}
